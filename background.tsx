7702911d52b94e80d21278ca9e86158f56244b9a
Emil <emil.winberg1@gmail.com>
2023-11-22 10:36:43 +0100

    Initial commit

---
+++ background.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="background" tilewidth="8" tileheight="8" tilecount="3888" columns="72">
+ <image source="img/background.png" width="576" height="432"/>
+</tileset>
