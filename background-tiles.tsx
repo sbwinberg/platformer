ef9c5a907bdb47c00a49235227db5a906521d6fd
Emil <emil.winberg1@gmail.com>
2024-11-06 09:00:45 +0100

    a

--- background-tiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="background" tilewidth="16" tileheight="16" tilecount="972" columns="36">
- <image source="img/background.png" width="576" height="432"/>
-</tileset>
